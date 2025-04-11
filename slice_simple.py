9b855368fdcb3217e8ccd692bc811d1f12caaa95
luciascott15 <lscott@mail.austral.edu.ar>
2025-04-11 12:22:52 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,5 @@
 def slice_simple():
     texto = "Awesome"
-    print(texto[:3].lower())
+    print(texto[:3])
     print(texto[2:5])
-    print(texto.lower())
+    print(texto[0:4]) + (texto [-3:-1])
