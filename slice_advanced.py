fd9eec699ad5a7ea6e73e45c06d486f87c2490a1
luciascott15 <lscott@mail.austral.edu.ar>
2025-04-11 12:29:31 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,4 @@
 def slice_advanced():
     texto = input("Ingresa texto:")
-    print(f"texto original: {texto}")
-    modificado = texto[4: :2]
-    print(f"texto modificado: {modificado}")
+    resultado = texto[4: :2]
+    print(resultado)
